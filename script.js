2487afbb15ab9cec748b1510946b0f8a57b8d05e
Kubanezya <niksan555bond@yandex.ru>
2024-06-26 14:24:23 +0300

    first

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+var swiper_mode = function(swp_disbl) {
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,  
+    slidesOffsetBefore: 15,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+swiper.destroy(swp_disbl, swp_disbl)
+}
+
+if (window.innerWidth>=768){
+  swp_disbl=true;
+  swiper_mode(swp_disbl);}
+else {
+  swp_disbl=false;
+  swiper_mode(swp_disbl)};  
+
+
+window.addEventListener('resize', () => {
+  var swp_disbl;
+  if (window.innerWidth>=768 & !swp_disbl){swiper_mode(swp_disbl);
+    console.log('off');
+    swp_disbl=true;
+  }
+  else {
+    swp_disbl=false;
+    swiper_mode(swp_disbl);
+    console.log('on')
+    console.log(swp_disbl);
+  }
+});
+
---
+++ script2.js
@@ -0,0 +1,50 @@
+const swiper = document.querySelector('.swiper');
+let mySwiper;
+
+function mobileSlider(){
+  if (window.innerWidth < 768 && swiper.dataset.mobile == 'false') {  
+    mySwiper = new Swiper(swiper, {
+    direction: 'horizontal',
+    slidesOffsetBefore: 16,  
+    loop: true,
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: ' true' ,
+    }
+});
+ swiper.dataset.mobile = 'true';
+}
+
+if (window.innerWidth >= 768) {
+  swiper.dataset.mobile = 'false';
+  
+  if(swiper.classList.contains('swiper-initialized')){
+    mySwiper.destroy(true, true); 
+    }
+  }
+}
+
+mobileSlider();
+
+window.addEventListener('resize', () => {
+  mobileSlider();
+ 
+});
+
+let btnMore = document.querySelector('.btn-more');
+let iconShow = document.querySelector('.icon-show');
+let swpHght = document.querySelector('.swiper');
+btnMore.onclick = function(){
+     if (btnMore.textContent == 'Показать все'){
+        btnMore.textContent='Скрыть';
+        iconShow.style.transform='rotate(180deg)';
+        swpHght.style.height='auto';
+    }
+     else{
+        btnMore.textContent='Показать все';
+        iconShow.style.transform='rotate(0deg)'
+        swpHght.style.height='200px';
+    }
+    };
